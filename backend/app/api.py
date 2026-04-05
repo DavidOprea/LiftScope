d73d19d7975762359286c35f3caab3e9d065cce0
David Oprea <opread012@gmail.com>
2026-04-05 12:46:32 -0400

    changes to backend files/folders to get Render backend working

--- backend/app/api.py
+++ backend/app/api.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import logs, image, profile
+from .routers import logs, image, profile
 
 app = FastAPI()
 app.include_router(logs.router)
