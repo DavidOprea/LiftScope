e75208445e5f0316fab96c140821ccec65a43148
David Oprea <opread012@gmail.com>
2026-04-26 12:17:46 -0400

    clear out branch

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Tell Metro to bundle .tflite files as raw assets
-config.resolver.assetExts.push('tflite');
-
-module.exports = config;
